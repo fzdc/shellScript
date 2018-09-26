e4d86969dc59c06231c7b6663fc30112ed8d411f
fzdc <455385478@qq.com>
2018-09-26 20:10:34 +0800

    day 5

--- if_1.sh
+++ if_1.sh
@@ -6,13 +6,13 @@
 #-lt:小于 lesser than
 #-le:小于等于 lesser or equal
 #-ge:大于等于 greater or equal
-
-use=`df -hT | grep "/boot" | awk '{print $6}' | cut -d "%" -f1`
+read -p "输入需要查看的盘符使用情况：" var
+use=`df -hT | grep -w $var | awk '{print $6}' | cut -d "%" -f1`
 #df -hT :查看磁盘使用情况
 #grep "/boot" :检索到/boot所在行
 #awk '{print $6}' : 输出第六个参数
 #cut -d "%" -f1 : 对第六个参数使用%进行切分(-d "%")，并取第一个参数值-f1
-
+echo $use
 if [ $use -gt 80 ];then
 	echo "Warning!!/boot disk is full"
 elif [ $use -gt 30 ];then
